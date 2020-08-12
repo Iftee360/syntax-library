e48e7b6430db09e1c23bfd850918ab94e3286293
Iftee360 <ifteeprogrammer360@gmail.com>
2020-08-12 20:38:33 +0600

    Create 3-casting.py

---
+++ Programming Languages/python/3-casting.py
@@ -0,0 +1,14 @@
+
+### CASTING and CONVERTING
+
+
+#float to integar
+print( int(3.14) )
+
+#integar to float
+print( float(3) )
+#boolean to string
+print( str(True) )
+
+#"string" to number
+print( int("50") + int("70") )
