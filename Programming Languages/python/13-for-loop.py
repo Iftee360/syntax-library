cb8a099f26dc33e22e62cee56855bdb768f6b77f
Iftee360 <ifteeprogrammer360@gmail.com>
2020-08-12 20:44:19 +0600

    Create 13-for-loop.py

---
+++ Programming Languages/python/13-for-loop.py
@@ -0,0 +1,15 @@
+### FOR LOOPS
+
+
+for index in range(5):
+    print(index)
+
+
+lucky_nums = [4, 8, 15, 16, 23, 42]
+
+for num in lucky_nums:
+    print(num)
+
+
+for letter in "iftee":
+    print(letter)
