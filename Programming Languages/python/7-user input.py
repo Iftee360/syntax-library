45e24edeec3d3d78aa19e9b70bbf64cef2c3487f
Iftee360 <ifteeprogrammer360@gmail.com>
2020-08-12 20:43:10 +0600

    Create 7-user input.py

---
+++ Programming Languages/python/7-user input.py
@@ -0,0 +1,11 @@
+### USER INPUT
+
+
+name = input("Enter your name: ")
+print("Hello", name + "!")
+
+#Every input is gonna be stored as a String, so you have to convert it into your desired data type...
+
+num1 = int(input("Enter First Num: "))
+num2 = int(input("Enter Second Num: "))
+print(num1 + num2)
