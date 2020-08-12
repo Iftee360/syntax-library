c7401728acd19c326ff84dabec4f2e0d6c3da142
Iftee360 <ifteeprogrammer360@gmail.com>
2020-08-12 20:38:24 +0600

    Create 2-variables & data types.py

---
+++ Programming Languages/python/2-variables & data types.py
@@ -0,0 +1,26 @@
+
+### VARIABLES and DATA TYPES
+
+
+'''
+Names are case-sensitive and may begin with:
+   letters, $, _
+After, may include
+   letters, numbers, $, _
+Convention says
+   Start with a lowercase word, then additional words are separated
+   by underscores
+   ex. my_first_variable
+'''
+
+name = "Iftee"    # Strings
+age = 30         # Integer
+gpa = 3.5         # Decimal
+is_tall = True    # Boolean -> True/False
+
+name = "John"
+
+# concatonate
+print("Your name is " + name)
+# C like
+print("Your name is", name)
