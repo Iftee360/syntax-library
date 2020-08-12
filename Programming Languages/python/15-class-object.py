5b950c3df7f3ac34312476bee827bd5e648996f9
Iftee360 <ifteeprogrammer360@gmail.com>
2020-08-12 21:41:43 +0600

    Create 15-class-object.py

---
+++ Programming Languages/python/15-class-object.py
@@ -0,0 +1,29 @@
+
+class Car:
+
+    # Constructor: constructs the required attributes of a class,
+    # self parameter is mendatory, which refers to the craeted object. Except that others are parameters you require
+
+    def __init__ (self, model, cost):            #initialize constructor
+        self.model = model
+        self.cost = cost
+
+    # Method: a function inside of a Class
+
+    def review_car(self):               #self is mendatory, which refers to the craeted object
+
+        print("The", self.model, "Car costs around", self.cost)
+
+        if self.cost >= 100000:
+            print("The car is costly, think before buying...\n")
+        else:
+            print("Price is within budget\n")
+
+
+
+
+car1 = Car("Audi A8", 280000)
+car1.review_car()
+
+car2 = Car("Toyota X Corolla", 70000)
+car2.review_car()
